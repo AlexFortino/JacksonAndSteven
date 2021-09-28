71198c9722804f7d141d511dc06b8705a99b3d24
steven404919 <91291645+steven404919@users.noreply.github.com>
2021-09-28 18:48:05 -0400

    Breaking the game

--- Project1/Assets/Scripts/TestGithub.cs
+++ Project1/Assets/Scripts/TestGithub.cs
@@ -7,7 +7,7 @@ public class TestGithub : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("gun");
     }
 
     // Update is called once per frame
