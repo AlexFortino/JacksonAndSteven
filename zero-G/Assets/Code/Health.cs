0f165292a3bf0fba479752f5941bb932cf2720b8
Jackson Beck <rsbst23@yahoo.com>
2022-01-23 18:06:39 -0500

    heath code

--- zero-G/Assets/Code/Health.cs
+++ zero-G/Assets/Code/Health.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Health : MonoBehaviour
 {
+    GameObject healthBar;
     public int health = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthBar = GameObject.FindGameObjectWithTag("Health UI");
     }
 
     // Update is called once per frame
@@ -19,6 +20,7 @@ public class Health : MonoBehaviour
     public void takeDamage(int dmg)
     {
         health = health - dmg;
+        healthBar.GetComponent<Text>().text=health.ToString();
         if (health <= 0)
         {
             Debug.Log("you are dead");
