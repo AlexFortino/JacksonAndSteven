faf32014e3b0b30e6c898d3eb94e69e433249ff4
Jackson Beck <rsbst23@yahoo.com>
2022-02-06 18:13:03 -0500

    zero g movement

--- zero-G/Assets/Code/gun.cs
+++ zero-G/Assets/Code/gun.cs
@@ -1,34 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gun : MonoBehaviour
 {
+    GameObject ammoBar;
+   
+
+    public int MaxAmmo = 20;
+    public int Ammo = 20;
+    public bool Reloading = false;
    public  GameObject Bullet;
     public GameObject muzzle;
 
     //  Start is called before the first frame update
     void Start()
+   
     {
-
+        ammoBar = GameObject.FindGameObjectWithTag("Ammo UI");
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
+        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger)&&Ammo>0)
 
         {
             Instantiate(Bullet, muzzle.transform.position, muzzle.transform.rotation);
-
-}
-        if (Input.GetMouseButtonDown(0))
-
-        {
-            Instantiate(Bullet, muzzle.transform.position, muzzle.transform.rotation);
-
+            Ammo -= 1;
+            ammoBar.GetComponent<Text>().text = Ammo.ToString();
+            if (Ammo == 0)
+            {
+                Invoke("IsReloading", 3);
+            }
         }
+        if (Input.GetMouseButtonDown(0)&&Ammo > 0)
+
+        {
+            Instantiate(Bullet, muzzle.transform.position, muzzle.transform.rotation);
+            Ammo -= 1;
+            ammoBar.GetComponent<Text>().text = Ammo.ToString();
+            if (Ammo == 0)
+            {
+                Invoke("IsReloading", 3);
+            }
+        }
+    }
+
+    void IsReloading()
+    {
+        Reloading = false;
+        Ammo = MaxAmmo;
+
+        ammoBar.GetComponent<Text>().text = Ammo.ToString();
+
     }
 
 }
--- zero-G/Assets/movemont.cs
+++ zero-G/Assets/movemont.cs
@@ -2,41 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movemont : MonoBehaviour
+    public class movemont : MonoBehaviour
 {
+
+    Vector2 dir;
     public float speed = 5;
     float horizontil = 0;
     float vericle = 0;
     Camera cam;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         horizontil += Input.GetAxis("Mouse X");
         vericle -= Input.GetAxis("Mouse Y");
-        transform.eulerAngles = new Vector3(0, horizontil, 0);
+        transform.eulerAngles = new Vector3(vericle, horizontil, 0);
         cam.transform.eulerAngles = new Vector3(vericle, horizontil, 0);
+
+        dir = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            //transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            rb.AddRelativeForce(Vector3.forward);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            //transform.Translate(Vector3.left * speed * Time.deltaTime);
+            rb.AddRelativeForce(Vector3.left);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back* speed * Time.deltaTime);
+            //transform.Translate(Vector3.back* speed * Time.deltaTime);
+            rb.AddRelativeForce(Vector3.back);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            //transform.Translate(Vector3.right * speed * Time.deltaTime);
+            rb.AddRelativeForce(Vector3.right);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //transform.Translate(Vector3.left * speed * Time.deltaTime);
+            rb.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            //transform.Translate(Vector3.back* speed * Time.deltaTime);
+            rb.AddRelativeForce(Vector3.down);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            //transform.Translate(Vector3.left * speed * Time.deltaTime);
+            rb.AddRelativeTorque(-Vector3.forward*2);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            //transform.Translate(Vector3.back* speed * Time.deltaTime);
+            rb.AddRelativeTorque(Vector3.forward*2);
         }
     }
-}
+} 
