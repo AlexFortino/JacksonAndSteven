c7fd99278bb6db946bcade56ec4c52e68dd053d6
steven404919 <91291645+steven404919@users.noreply.github.com>
2022-02-06 19:00:51 -0500

    Hand movement testing

---
+++ zero-G/Assets/Hand.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        player.transform.parent = gameObject.transform;
+    }
+
+}
